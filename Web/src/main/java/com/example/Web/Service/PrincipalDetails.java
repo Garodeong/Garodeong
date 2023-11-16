a17d3731cd11534eb802a56ba8a46b250a1fe59d
silver00-psh <silveroo00@naver.com>
2023-11-16 14:53:21 +0900

    [Backend] add PrincipalOauth2UserService

--- Web/src/main/java/com/example/Web/Service/PrincipalDetails.java
+++ Web/src/main/java/com/example/Web/Service/PrincipalDetails.java
@@ -3,15 +3,30 @@ package com.example.Web.Service;
 import com.example.Web.Domain.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayDeque;
 import java.util.Collection;
+import java.util.Map;
 
-public class PrincipalDetails implements UserDetails {
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
    private User user;
+   private Map<String, Object> attributes;
 
-   public PrincipalDetails(User user) { this.user = user; }
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+   public PrincipalDetails(User user, Map<String , Object> attributes) {
+       this.user = user;
+       this.attributes = attributes;
+   }
+
+   @Override
+   public String getName() { return null; }
+   @Override
+   public Map<String, Object> getAttributes() { return attributes; }
 
    //권한 관련 작업을 하기 위한 role return
     @Override
